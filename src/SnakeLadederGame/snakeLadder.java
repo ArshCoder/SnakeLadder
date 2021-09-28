adac7f7355bed2a23925f59460fba126c6e0cfea
CoderArsh <arshjeetb08@gmail.com>
2021-09-28 22:35:48 +0530

    Added: UC3 To check for Option

--- src/SnakeLadederGame/snakeLadder.java
+++ src/SnakeLadederGame/snakeLadder.java
@@ -1,12 +1,30 @@
 package SnakeLadederGame;
 
 public class snakeLadder {
-	public static void main( String args[] ) {
-	      int min = 1;
-	      int max = 6;
-	      
-	      int int_random = (int)Math.floor(Math.random()*(max-min+1)+min);  
-	      System.out.println("Dice Rolled You Get : " + int_random);
-	      }
+	private static final int NO_PLAY = 0;
+	private static final int LADDER = 1;
+	private static final int SNAKE = 2;
+
+	public static void main(String[] args) {
+
+		int startposition = 0;
+
+		int option = (int) Math.floor(Math.floor(Math.random() * 10) % 3);
+		int diceRoll = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+		System.out.println("Dice Rolled You Get : " + diceRoll);
+
+		if (option == LADDER) {
+			startposition += diceRoll;
+			System.out.println("You got ladder" + startposition);
+		} else if (option == SNAKE) {
+			startposition -= diceRoll;
+			System.out.println("You Bitten By Snake " + startposition);
+		} else if (option == NO_PLAY) {
+
+			System.out.println("No Play for You " + startposition);
+
+		}
+
+	}
 
 }
