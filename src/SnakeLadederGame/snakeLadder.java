85670b4ea41009da61f501c586bd0d990981732b
CoderArsh <arshjeetb08@gmail.com>
2021-09-28 22:25:04 +0530

    Added: UC1 Start the Game

---
+++ src/SnakeLadederGame/snakeLadder.java
@@ -0,0 +1,9 @@
+package SnakeLadederGame;
+
+public class snakeLadder {
+	public static void main(String[] args) {
+		int starting_point = 0;
+		System.out.println("Single Player Starting Point is : " + starting_point);
+		}
+
+}
