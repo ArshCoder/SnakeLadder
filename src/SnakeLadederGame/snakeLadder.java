72274bf53877a6bccb099900519d2fe6fe53c56e
CoderArsh <arshjeetb08@gmail.com>
2021-09-28 22:31:49 +0530

    Added:UC2 Roll the Dice

--- src/SnakeLadederGame/snakeLadder.java
+++ src/SnakeLadederGame/snakeLadder.java
@@ -1,9 +1,12 @@
 package SnakeLadederGame;
 
 public class snakeLadder {
-	public static void main(String[] args) {
-		int starting_point = 0;
-		System.out.println("Single Player Starting Point is : " + starting_point);
-		}
+	public static void main( String args[] ) {
+	      int min = 1;
+	      int max = 6;
+	      
+	      int int_random = (int)Math.floor(Math.random()*(max-min+1)+min);  
+	      System.out.println("Dice Rolled You Get : " + int_random);
+	      }
 
 }
